3449b96045a47393ade266b30d8ff78407b6b6ce
KOHAN <alko5@EVIRL-011-OK.dsic.cloud>
2017-10-16 20:29:04 +0200

    FIX

---
+++ include/asin.h
@@ -0,0 +1,107 @@
+/* A Bison parser, made by GNU Bison 2.7.  */
+
+/* Bison interface for Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989-1990, 2000-2012 Free Software Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_ASIN_H_INCLUDED
+# define YY_YY_ASIN_H_INCLUDED
+/* Enabling traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     ID_ = 258,
+     CTE_ = 259,
+     OPMAS_ = 260,
+     OPMULT_ = 261,
+     COMEN_ = 262,
+     PABIERTO_ = 263,
+     PCERRADO_ = 264,
+     LABIERTA_ = 265,
+     LCERRADA_ = 266,
+     LEER_ = 267,
+     IMPRIMIR_ = 268,
+     TRUE_ = 269,
+     FALSE_ = 270,
+     FOR_ = 271,
+     WHILE_ = 272,
+     IF_ = 273,
+     ELSE_ = 274,
+     ELSEIF_ = 275,
+     DO_ = 276,
+     ASIG_ = 277,
+     OPREST_ = 278,
+     OPDIV_ = 279,
+     OPMOD_ = 280,
+     MAYQ_ = 281,
+     MENQ_ = 282,
+     FINL_ = 283,
+     CORA_ = 284,
+     CORC_ = 285,
+     NEG_ = 286,
+     AND_ = 287,
+     OR_ = 288
+   };
+#endif
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+extern YYSTYPE yylval;
+
+#ifdef YYPARSE_PARAM
+#if defined __STDC__ || defined __cplusplus
+int yyparse (void *YYPARSE_PARAM);
+#else
+int yyparse ();
+#endif
+#else /* ! YYPARSE_PARAM */
+#if defined __STDC__ || defined __cplusplus
+int yyparse (void);
+#else
+int yyparse ();
+#endif
+#endif /* ! YYPARSE_PARAM */
+
+#endif /* !YY_YY_ASIN_H_INCLUDED  */
---
+++ src/asin.y
@@ -0,0 +1,135 @@
+%{
+ #include <stdio.h>
+ extern int yylineno ;
+ extern FILE *yyin ;
+ int numErrores ;
+%}
+%error-verbose
+%token ID_ CTE_ OPMAS_ OPMULT_ COMEN_ PABIERTO_ PCERRADO_ LABIERTA_ LCERRADA_ 
+LEER_ IMPRIMIR_ TRUE_ FALSE_ FOR_ WHILE_ IF_ ELSE_ ELSEIF_ DO_ ASIG_ OPREST_ OPDIV_ 
+OPMOD_ MAYQ_ MENQ_  FINL_ CORA_ CORC_ NEG_ AND_ OR_
+%%
+
+programa: LABIERTA_ secuenciaSentencias LCERRADA_
+;
+
+declaracion: tipoSimple ID_ FINL_
+| tipoSimple ID_ CORA_ CTE_ CORC_ FINL_
+;
+
+listaInstrucciones: listaInstrucciones instruccion
+|
+;
+
+instruccionSeleccion: IF_ PABIERTO_ expresion PCERRADO_ instruccion restoIf
+;
+
+expresion: expresionLogica
+| ID_ operadorAsignacion expresion
+| ID_ CORA_ expresion CORC_ operadorAsignacion expresion
+;
+
+expresionRelacional: expresionAditiva
+| expresionRelacional operadorRelacional expresionAditiva
+;
+
+expresionUnaria: expresionSufija
+| operadorUnario expresionUnaria
+| operadorIncremento ID_
+;
+
+operadorLogico: AND_ AND_
+| OR_ OR_
+;
+
+operadorAditivo: OPMAS_
+| OPREST_
+;
+
+operadorIncremento: OPMAS_ OPMAS_
+| OPREST_ OPREST_
+;
+
+sentencia: declaracion
+			| instruccion
+			;
+
+instruccion: LABIERTA_ listaInstrucciones LCERRADA_ |
+			| instruccionEntradaSalida
+			| instruccionExpresion | instruccionSeleccion
+			| instruccionIteracion
+			;
+
+instruccionEntradaSalida: LEER_ PABIERTO_ ID_ PCERRADO_ FINL_
+			| IMPRIMIR_ PABIERTO_ expresion PCERRADO_ FINL_
+			;
+
+instruccionIteracion: WHILE_ PABIERTO_ expresion PCERRADO_ instruccion
+			| DO_ instruccion WHILE_ PABIERTO_ expresion PCERRADO_
+			;
+			
+expresionIgualdad: expresionRelacional
+			| expresionIgualdad operadorIgualdad expresionRelacional
+			;
+			
+expresionMultiplicativa: expresionUnaria
+			| expresionMultiplicativa operadorMultiplicativo expresionUnaria
+			;
+
+operadorAsignacion: ASIG_ | OPMAS_ ASIG_ | OPREST_ ASIG_ | OPMULT_ ASIG_ | OPDIV_ ASIG_ ;
+
+operadorRelacional: MAYQ_ | MENQ_ | MAYQ_ ASIG_ | MENQ_ ASIG_ ;
+
+operadorUnario: OPMAS_ | OPREST_ | NEG_ ;
+
+secuenciaSentencias: sentencia
+	| secuenciaSentencias sentencia
+	;
+	
+tipoSimple: CTE_
+	| TRUE_
+	| FALSE_
+	;
+	
+instruccionExpresion: expresion FINL_
+	| FINL_
+	;
+	
+restoIf: ELSEIF_ PABIERTO_ expresion PCERRADO_ instruccion restoIf
+	| ELSE_ instruccion
+	;
+
+expresionLogica: expresionIgualdad
+	|	expresionLogica operadorAsignacion expresionIgualdad
+	;
+	
+expresionAditiva: expresionMultiplicativa 
+	|	expresionAditiva operadorAditivo expresionMultiplicativa
+	;
+	
+expresionSufija: PABIERTO_ expresion PCERRADO_
+	| ID_ operadorIncremento
+	| ID_ CORA_ expresion CORC_
+	;
+
+operadorIgualdad: ASIG_ ASIG_
+	| NEG_ ASIG_
+	;
+	
+operadorMultiplicativo: OPMULT_
+	| OPDIV_
+	| OPMOD_
+	;
+
+%%
+
+/* Llamada a yyparse ante un error */
+/*void yyerror (char *msg) {
+	numErrores++;
+	fprintf(stdout, "\n>Error at line %d: %s\n", yylineno, msg);
+}
+int main (int argc, char **argv){
+	 if ((yyin = fopen (argv[1], "r")) == NULL)
+		fprintf (stderr, "Fichero no valido \%s", argv[1]);
+	 yyparse();
+} */
